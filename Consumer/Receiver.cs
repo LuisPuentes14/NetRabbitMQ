8b280ccc60d3a3174a467c4676c8640026535136
AlejandroPuentes <alejandropuentes@wposs.com>
2024-05-21 14:53:27 -0500

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,36 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "alejandro",
+    Password = "12345",
+};
+
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+
+    channel.QueueDeclare("VaxiQueue", false, false, false, null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.Span;
+        var message = Encoding.UTF8.GetString(body);
+
+        Console.WriteLine("Mensaje recibido {0}",message);      
+       
+    };
+    // una ves recibido se quita del Queue, simpre y cuando 
+    // el segundo parametro este en true, si esta en falso no lo quita
+    channel.BasicConsume("VaxiQueue", true, consumer);
+
+}
+
+Console.WriteLine("Presiona [Enter] para salir de la aplicacion");
+Console.ReadLine();
---
+++ Producer/Sender.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "alejandro",
+    Password = "12345",
+};
+
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel()) {
+
+    channel.QueueDeclare("VaxiQueue", false, false, false, null);
+
+    var message = "Bienvenido al curso de RabbitMQ y .NET";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "VaxiQueue",null, body );
+
+    Console.WriteLine("El mensaje fue enviado {0}", message);
+    
+}
+
+Console.WriteLine("Presiona [Enter] para salir de la aplicacion");
+Console.ReadLine();
